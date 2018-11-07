a97790c7f019894fcae9275a66adfe9b8f3c4112
HelloNoise <mariakuikka@hotmail.com>
2018-11-07 19:41:17 +0200

    message logged when 3 parts of each color are collected

--- Assets/Scripts/DropZone.cs
+++ Assets/Scripts/DropZone.cs
@@ -1,8 +1,17 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 using System.Collections;
 using UnityEngine.EventSystems;
 
+
+
 public class DropZone : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler {
+
+    public List<string> Reds = new List<string>();
+    public List<string> Blues = new List<string>();
+    public List<string> Greens = new List<string>();
+
+    //public List<string> Colors = new List<string>(new string[] { "red", "blue", "green" });
 
     public void OnPointerEnter(PointerEventData eventData) {
 
@@ -29,7 +38,35 @@ public class DropZone : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPoin
     }
 
     public void OnDrop(PointerEventData eventData) {
-        Debug.Log(eventData.pointerDrag.name + " dropped on " + gameObject.name);
+        //Random.Range(0, 4);
+        // Debug.Log(eventData.pointerDrag.name + " dropped on " + gameObject.name + " tag: " + eventData.pointerDrag.tag);
+
+        switch (eventData.pointerDrag.tag)
+        {
+            case "red":
+                Reds.Add(eventData.pointerDrag.tag);
+                if (Reds.Count == 3) {
+                    Debug.Log("red " + Reds.Count);
+                }
+                break;
+            case "blue":
+                Blues.Add(eventData.pointerDrag.tag);
+                if (Blues.Count == 3)
+                {
+                    Debug.Log("blue " + Blues.Count);
+                }
+                break;
+            case "green":
+                Greens.Add(eventData.pointerDrag.tag);
+                if (Greens.Count == 3)
+                {
+                    Debug.Log("green " + Greens.Count);
+                }
+                break;
+            default:
+                Debug.Log("color not found");
+                break;
+        }
 
         Draggable d = eventData.pointerDrag.GetComponent<Draggable>();
         if (d != null) {
